47ab52cb752a4e028afe942823380e8c3d54e56f
Peter Bikundo <binmonk@gmail.com>
2023-04-13 15:16:26 +0300

    add db dumpfile  - clean up

--- dumpfile.sql
+++ dumpfile.sql
@@ -4,7 +4,7 @@
 -- https://tableplus.com/
 --
 -- Database: mailerlite
--- Generation Time: 2023-04-13 14:51:24.1220
+-- Generation Time: 2023-04-13 15:15:32.0080
 -- -------------------------------------------------------------
 
 
@@ -70,15 +70,6 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2023_04_11_073634_create_settings_table', 1);
-
-INSERT INTO `settings` (`id`, `key`, `value`, `created_at`, `updated_at`) VALUES
-(1, 'mailerlite_api_token', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiYzJlMGU2ODA5Yzg5Y2M1Y2IyOGVlMzkxYzA1YWMyNmM0MDU5NmIyODRiOTMwNzVjMDg3MmMyYjBmNzk1MTQzY2ZjMzE3Y2Y1MGE2NWJjYjEiLCJpYXQiOjE2ODExMzc3NTUuMTI2MjQsIm5iZiI6MTY4MTEzNzc1NS4xMjYyNDIsImV4cCI6NDgzNjgxMTM1NS4xMTkxNCwic3ViIjoiNDI1NTUwIiwic2NvcGVzIjpbXX0.vSCOhekspJfK2IR2CEy8nRtRTvhCoyyR0cfX6ph_1qQVAwtDH39vxhP2qONK3tIjYyrmf4z4H-PCkk4H_03IsKsuF0c0u1EP8W1EuWvR4d5G539-QOBtZ9BnADXgWPb9bIXE5rVb33qU0AQfHK2D7lfh2Xm7ZaBS04N_ZJc81ML6eNoVfi5FJZZnp0IkqtECnQJpJvYnFVT-usDduZ436NXmP5Y0HKk9lg15p19l-sh-AM4H596RSYwZKThnT_rPV20VK6MbZyUlx1XUqp2fsUgQacA4AkQwU5lR6cA8GM7WM4TR2DuY0z7Xx-Z4aKX1hnfDZkoT6NnLtI9Zzl_zjwlq2CPRpfr9-QjYra3Mhi7AdNHPFAEG29e7DnMsqrQD0faXQqD0RZpTw0kZAU8gU94rl7TFSyEZlhDa3h0zJtybJ4EwvNLdZ5TaLnDxh2LdWDI8t1x9VQofp4qr1A9XCAOyltHUcLJgsBS6JVFlz-lkAbEQcTT8cIHkO4eF9sddHzCmqTCMhuRdHpp1vGCYQtDn43VoAZ6hCpduq-eLbj4XdLrk97j-CALhkaiOZL9r8r1vWCaxaEM5KJ2v26rHz2visVZmvJIfQvxKAQj837JhS3Bqx5comtg8BCystpnZcSBluJa8OOzrZA71iReQQSbLI9d59SpeBkGBv8s9T6I', NULL, NULL);
 
 
 
